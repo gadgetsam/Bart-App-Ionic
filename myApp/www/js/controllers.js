2b5505f1f72c846c9f0c97b9a0d3c5b21f5afce8
Sam Schickler <gadgetsam@gmail.com>
2015-12-20 13:20:21 -0800

    fixed depart error

--- myApp/www/js/controllers.js
+++ myApp/www/js/controllers.js
@@ -187,7 +187,7 @@ angular.module('starter.controllers', [])
                 //console.log(dict+"123");
                 orgin = dict[j][0]
                 dest = dict[j][1]
-                $http.get('http://api.bart.gov/api/sched.aspx?cmd=arrive&orig=' + orgin + '&dest=' + dest + '&date=now&key=MW9S-E7SL-26DU-VV8V&b=2&a=2&l=1').then(function (string) {
+                $http.get('http://api.bart.gov/api/sched.aspx?cmd=depart&orig=' + orgin + '&dest=' + dest + '&date=now&key=MW9S-E7SL-26DU-VV8V&b=2&a=2&l=1').then(function (string) {
                     //console.log('Success', string);
 
                     tripobj = xmlParser.xml_str2json(string.data);
